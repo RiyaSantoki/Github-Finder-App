6f3a91db70122fb6f9cb9b26109957735bb70876
RiyaSantoki <riyasantoki28@gmail.com>
2022-01-15 09:52:06 +0530

    Github-Finder-App
    Github-Finder-App in React Js

---
+++ src/pages/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            <h1 className="text-6xl mb-4">GitHub Finder</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam voluptatibus aspernatur repellendus suscipit ducimus, nemo aliquid ea dignissimos tempora inventore, accusantium fugit quaerat? Doloremque atque, non vero ducimus architecto aut, labore aperiam et enim, blanditiis ea laudantium optio incidunt quos repudiandae fugiat sit inventore ipsum itaque facilis autem. Dicta, porro!</p>
+            <p className='text-lg text-gray-400'>Version <span className='text-white'>1.0.0</span></p>
+
+            <p className='text-lg text-gray-400'>Layout By:   <span className='text-white'>
+                Riya Santoki
+            </span>
+            </p>
+
+
+        </div>
+    )
+}
+
+export default About
